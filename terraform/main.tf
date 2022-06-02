b8422e377c7c3a43d57135dfc97e5edfbac4e2aa
Student1 ASD <Student1-12135712@lodsasdoutlook.onmicrosoft.com>
2022-06-02 16:55:18 -0700

    Add terraform code [skip ci]

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+data "azurerm_resource_group" "rg" {
+  name = "rg-lod23818553"
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "azurerm_mssql_server" "server" {
+  name                         = "mssqlserver"
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  location                     = data.azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = random_password.password.result
+  minimum_tls_version          = "1.2"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "7b250d58-2629-4e60-a69a-e3eedb5f720f"
+  }
+}
+
+resource "azurerm_mssql_database" "db" {
+  name           = "mydb"
+  server_id      = azurerm_mssql_server.server.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+}
