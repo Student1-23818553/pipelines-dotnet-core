9eeec67bf3a6f46154ecc8aefbb91d8021ef9846
Phil White <Student1-23818553@outlook.com>
2022-06-02 21:38:50 -0400

    Update terraform [skip ci]

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "rg" {
-  name = "rg-lod23818553"
+  name = "myrg"
 }
 
 resource "random_password" "password" {
@@ -9,7 +9,7 @@ resource "random_password" "password" {
 }
 
 resource "azurerm_mssql_server" "server" {
-  name                         = "mssqlserver"
+  name                         = "student1-23818553"
   resource_group_name          = data.azurerm_resource_group.rg.name
   location                     = data.azurerm_resource_group.rg.location
   version                      = "12.0"
@@ -19,7 +19,7 @@ resource "azurerm_mssql_server" "server" {
 
   azuread_administrator {
     login_username = "AzureAD Admin"
-    object_id      = "7b250d58-2629-4e60-a69a-e3eedb5f720f"
+    object_id      = "6045736a-0263-4c14-ac9a-faed11e12a0d"
   }
 }
 
@@ -28,8 +28,7 @@ resource "azurerm_mssql_database" "db" {
   server_id      = azurerm_mssql_server.server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
-  read_scale     = true
+  read_scale     = false
   sku_name       = "S0"
-  zone_redundant = true
+  zone_redundant = false
 }
