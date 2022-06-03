40f35331673b7c21abbaf4695b26d5727b5dad33
Phil White <Student1-23818553@outlook.com>
2022-06-02 22:40:01 -0400

    Running fmt [skip ci]

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.8.0"
     }
   }
@@ -15,5 +15,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  skip_provider_registration= true
+  skip_provider_registration = true
 }
